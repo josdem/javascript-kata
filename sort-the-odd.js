a957298717fbab0586399cc5b98dd22dbc4e65e3
josdem <joseluis.delacruz@gmail.com>
2021-06-21 20:37:07 -0400

    Adding sort the odd kata solution

--- sort-the-odd.js
+++ sort-the-odd.js
@@ -1,4 +1,10 @@
 const sort = (array) => {
+    const odds = array.filter(element => element % 2 === 1 || element % 2 === -1).sort((a, b) => a - b)
+
+    for(let i = 0, j = 0; i < array.length ; i++){
+        array[i] = array[i] % 2 === 0 ? array[i] : odds[j++]
+    }
+     
     return array
 }
 
--- test/sort-the-odd-test.js
+++ test/sort-the-odd-test.js
@@ -2,12 +2,18 @@ const { assert } = require('chai')
 const { sorter } = require('../sort-the-odd.js')
 
 describe('Sorting odd numbers', () => {
+    
     it('it sort an empty array', () => {
-        assert.deepEqual(sorter.sort([]), [])
+        assert.deepEqual(sorter([]), [])
     })
 
     it('it sort some arrays', () => {
         assert.deepEqual(sorter([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
         assert.deepEqual(sorter([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
+    })
+
+    it('it sort special arrays', () => {
+        assert.deepEqual(sorter([-5, 17, -16]), [-5, 17, -16])
+        assert.deepEqual(sorter([1, 5, 2, 8, 3, 4, 11]), [1, 3, 2, 8, 5, 4, 11])
     })
 })
