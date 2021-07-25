39bbe7bb402c0dfd09e7c903e9b26dcf401b5c9e
josdem <joseluis.delacruz@gmail.com>
2021-07-24 21:12:14 -0400

    Adding find the odd kata approach

--- js/find-the-odd.js
+++ js/find-the-odd.js
@@ -4,6 +4,9 @@ There will always be only one integer that appears an odd number of times.
 */
 
 const oddFinder = (array) => {
+    let map = new Map()
+    array.filter(it => map.set(it, array.filter(n => n == it).length))
+    console.log(map)
     return array.length
 }
 
