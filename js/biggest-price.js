0437ac58bd6e43c3e0f4f46a5e20169c45aec99f
josdem <joseluis.delacruz@gmail.com>
2021-07-27 20:44:23 -0400

    Adding biggest price kata description

--- js/biggest-price.js
+++ js/biggest-price.js
@@ -1,3 +1,11 @@
+
+/*
+Messages with random data are coming! But we just care about prices!
+Your task is to implement a function which removes all non numeric data and return just the biggest price in the collection
+messages = ["hi", "2.0", "@#$%", "32.0"]
+result = 32.0
+*/
+
 const biggest = () => {
     return 0.0
 }
