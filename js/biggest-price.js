bda330c04df3d171605adbf6e7e989f3ea43e335
josdem <joseluis.delacruz@gmail.com>
2021-07-28 20:51:39 -0400

    Removing float numbers

--- js/biggest-price.js
+++ js/biggest-price.js
@@ -2,7 +2,7 @@
 /*
 Messages with random data are coming! But we just care about prices!
 Your task is to implement a function which removes all non numeric data and return just the biggest price in the collection
-messages = ["hi", "2.0", "@#$%", "32.0"]
+messages = ["hi", "2", "@#$%", "32"]
 result = 32.0
 */
 
--- test/biggest-price-test.js
+++ test/biggest-price-test.js
@@ -4,8 +4,8 @@ const {biggest} = require('../js/biggest-price.js')
 describe('it should select biggest price', () => {
 
     it('get biggest price', () => {
-        assert.strictEqual(biggest(['hi', '2.0', '@#$%', '32.0']), 32)
-        assert.strictEqual(biggest(['pair', 'car', '-10.0']), -10)
+        assert.strictEqual(biggest(['hi', '2', '@#$%', '32']), 32)
+        assert.strictEqual(biggest(['pair', 'car', '-10']), -10)
         assert.strictEqual(biggest(['main', '3.14', 'yaml']), 3.14)
     })
 })
