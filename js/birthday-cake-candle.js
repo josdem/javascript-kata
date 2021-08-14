298212b507d73c5c113f1789477aaeb322ff6563
josdem <joseluis.delacruz@gmail.com>
2021-08-14 07:36:14 -0400

    Adding coding challenge definition

--- js/birthday-cake-candle.js
+++ js/birthday-cake-candle.js
@@ -1,3 +1,7 @@
+/*
+It is your birthday! And you want to make it special, so you want to keep only biggest candles in the cake
+Your task is to create a function that removes all small candles and just keep the biggest ones.
+ */
 
 const counter = (candles) => {
     return candles.filter(it => it == Math.max(...candles))
