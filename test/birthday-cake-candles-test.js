ef79e667eb4ed61b0d8ba868f7f7f502f7874e02
josdem <joseluis.delacruz@gmail.com>
2021-08-14 06:58:01 -0400

    Changing strict equal by deep equal

--- test/birthday-cake-candles-test.js
+++ test/birthday-cake-candles-test.js
@@ -4,8 +4,8 @@ const {counter} = require('../js/birthday-cake-candle.js')
 describe('Keep biggest candles', () => {
 
     it('validates biggest candles', () => {
-        assert.strictEqual(counter([]), [])
-        assert.strictEqual(counter([1]), [1])
-        assert.strictEqual(counter([3, 2, 1, 3]), [3, 3])
+        assert.deepEqual(counter([]), [])
+        assert.deepEqual(counter([1]), [1])
+        assert.deepEqual(counter([3, 2, 1, 3]), [3, 3])
     })
 })
