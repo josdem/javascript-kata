adc2e6afb327245b5982f05feccd10c9cd18a765
josdem <joseluis.delacruz@gmail.com>
2021-08-14 07:26:28 -0400

    Adding birthday cake candles kata solution

--- js/birthday-cake-candle.js
+++ js/birthday-cake-candle.js
@@ -1,6 +1,6 @@
 
 const counter = (candles) => {
-    return candles
+    return candles.filter(it => it == Math.max(...candles))
 }
 
 module.exports.counter = counter
--- test/birthday-cake-candles-test.js
+++ test/birthday-cake-candles-test.js
@@ -4,7 +4,6 @@ const {counter} = require('../js/birthday-cake-candle.js')
 describe('Keep biggest candles', () => {
 
     it('validates biggest candles', () => {
-        assert.deepEqual(counter([]), [])
         assert.deepEqual(counter([1]), [1])
         assert.deepEqual(counter([3, 2, 1, 3]), [3, 3])
     })
