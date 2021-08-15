605d3d319067999f7311962527ef8f6b32189181
josdem <joseluis.delacruz@gmail.com>
2021-08-15 09:20:56 -0400

    Adding sum a collection kata

---
+++ test/collection-adder-test.js
@@ -0,0 +1,10 @@
+const {assert} = require('chai')
+const {sum} = require('../js/collection-adder.js')
+
+describe('should sum a collection', () => {
+    
+    it('verify collection sum', () => {
+        assert.deepEqual(21, sum([5, 4, 1, 2, 9]))
+        assert.deepEqual(31, sum([1, 2, 3, 4, 10, 11]))
+    })
+})
