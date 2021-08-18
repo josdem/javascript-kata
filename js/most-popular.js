b03cece34aa431da5d640fd7d1086807fd00a7a0
josdem <joseluis.delacruz@gmail.com>
2021-08-17 21:40:17 -0400

    Adding most popular kata

---
+++ js/most-popular.js
@@ -0,0 +1,10 @@
+/*
+Given a collection with numbers: [34 , 31, 34, 56, 12, 35, 24, 34, 69, 18]
+Write a function that finds most popular number in the array, in this case 34 because it is the number that appears most often.
+ */
+
+const find = (numbers) => {
+    return numbers.length
+}
+
+module.exports.find = find
---
+++ test/most-popular-test.js
@@ -0,0 +1,9 @@
+const {assert} = require('chai')
+const {find} = require('../js/most-popular')
+
+describe('Should find most popular', () => {
+
+    it('finds most popular',() => {
+        assert.deepEqual(34, find([34 , 31, 34, 56, 12, 35, 24, 34, 69, 18]))
+    })
+})
