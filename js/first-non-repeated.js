150f073b7f79f814bc6e65e5be127996a6df1dd2
josdem <joseluis.delacruz@gmail.com>
2021-08-24 08:17:52 -0400

    Adding first non repeated kata solution

--- js/first-non-repeated.js
+++ js/first-non-repeated.js
@@ -1,5 +1,14 @@
 const find = (numbers) => {
-    return numbers.length
+
+    const uniques = []
+
+    return numbers.reduce((accomulator, it) => {
+        accomulator = numbers.filter(number => number === it).length
+        if (accomulator === 1) {
+            uniques.push(it)
+        }
+        return uniques[0]
+    })
 }
 
 module.exports.find = find
