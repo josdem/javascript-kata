3274c7381f6e10d3342b80c4501c7a1c220b2ef0
josdem <joseluis.delacruz@gmail.com>
2021-08-24 08:19:45 -0400

    Adding first non repeated kata description

--- js/first-non-repeated.js
+++ js/first-non-repeated.js
@@ -1,3 +1,8 @@
+/*
+Given a numbers collection find fist non-repeated number in that collection
+When we have: [25, 25, 18, 18, 18, 36, 1, 2, 2, 34] first non-repeated number is 36
+ */
+
 const find = (numbers) => {
 
     const uniques = []
