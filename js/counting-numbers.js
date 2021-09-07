25e843a69b74d283cbe32e75b51cd9a60cc10982
josdem <joseluis.delacruz@gmail.com>
2021-09-06 22:24:52 -0500

    Adding counting numbers kata solution

--- js/counting-numbers.js
+++ js/counting-numbers.js
@@ -1,5 +1,11 @@
+/*
+Given an integer collection, return an array with three elements:
+how many of them are positive, how many negative and how many are zeros.
+Given: [-4, 3, -9, 0, 4, 1] then expected output is: [3, 2, 1]
+ */
+
 const counter = (numbers) => {
-  return numbers
+  return [numbers.filter((it) => it > 0).length, numbers.filter((it) => it < 0).length, numbers.filter((it) => it == 0).length]
 }
 
 exports.counter = counter
