c8079d52003e0fef3ca53a7b23d3b61f534ce1c0
josdem <joseluis.delacruz@gmail.com>
2021-11-09 14:57:21 -0500

    Adding common elements challenge

---
+++ test/common-elements-test.js
@@ -0,0 +1,10 @@
+const {assert} = require("chai")
+const {finder} = require("../js/common-elements")
+
+describe("should select elements in common", () => {
+
+    it("filters items in common", () => {
+        assert.deepEqual(finder([1,2,3,4,5], [1,3,5,7,9]), [1,3,5])
+    })
+
+})
