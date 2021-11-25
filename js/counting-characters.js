970ea3451d30f58f8d65aa3165594ad2a2a3712f
josdem <joseluis.delacruz@gmail.com>
2021-11-25 14:04:46 -0600

    Adding counting consonants solution

--- js/counting-characters.js
+++ js/counting-characters.js
@@ -4,7 +4,8 @@ const countVowels = (string) => {
 }
 
 const countConsonants = (string) => {
-    return string.length
+    let consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "r", "p", "q", "s", "t", "v", "w", "x", "y", "z"]
+    return Array.from(string).filter(it => consonants.includes(it)).length
 }
 
 module.exports.counter = {
