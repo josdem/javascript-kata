76fc0bb89e9d23591ccf2366256517287e484f52
josdem <joseluis.delacruz@gmail.com>
2021-11-25 15:55:47 -0600

    Adding counting consonants kata description

--- js/counting-characters.js
+++ js/counting-characters.js
@@ -1,3 +1,10 @@
+/*
+* Counting vowels and consonants. Create two functions one for counting vowels and another for
+* Given: A string
+* When: That string is josdem
+* Then: Counting vowels should be 2 and consonants 4
+*/
+
 const countVowels = (string) => {
     let vowels = ["a", "e", "i", "o", "u"]
     return Array.from(string).filter(it => vowels.includes(it)).length
