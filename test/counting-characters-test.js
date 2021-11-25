33fc5c44ba64b83d4ae5fdebe68ee68ab1314bbe
josdem <joseluis.delacruz@gmail.com>
2021-11-25 16:09:04 -0600

    Setting nickname as a constant

--- test/counting-characters-test.js
+++ test/counting-characters-test.js
@@ -2,14 +2,14 @@ const { assert } = require("chai")
 const { counter } = require("../js/counting-characters")
 
 describe("should count vowels and consonants", () => {
+
+    const nickname = "josdem"
     
     it("counts vowels", () => {
-        let nickname = "josdem"
         assert.deepEqual(2, counter.countVowels(nickname), "should count two vowels")
     })
 
     it("counts consonants", () => {
-        let nickname = "josdem"
         assert.deepEqual(4, counter.countConsonants(nickname), "should count four vowels")
     })
 })
