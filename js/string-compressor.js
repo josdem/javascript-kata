32c58177da38b2eb9d4f2ca6bdc7f97b25cd44d8
josdem <joseluis.delacruz@gmail.com>
2021-12-29 19:00:39 -0600

    Adding string compressor kata solution

--- js/string-compressor.js
+++ js/string-compressor.js
@@ -5,7 +5,17 @@ Then we have a compressed string like "a3b4c2"
  */
 
 const compress = (keyword) => {
-    return keyword
+    let result = ""
+    const map = new Map()
+    Array.from(keyword).reduce((acumulator, it) => {
+        let counter = Array.from(keyword).filter(number => number == it).length
+        map.set(it, counter)
+        acumulator = it
+    })
+    map.forEach((key, value) => {
+        result = result.concat(value, key)
+    })
+    return result
 }
 
 exports.compress = compress
