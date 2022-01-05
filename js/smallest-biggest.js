5b0809a3e3bb8e1ee1156f9e43fdc31788352d9b
josdem <joseluis.delacruz@gmail.com>
2022-01-05 08:57:13 -0600

    Adding smallest biggest kata solution

--- js/smallest-biggest.js
+++ js/smallest-biggest.js
@@ -1,5 +1,12 @@
+/*
+Find smaller and biggest numbers in a collection
+Given: A collection like [7, 5, 2, 4, 3, 9]
+When: I call find method
+Then: I will get a collection with [2, 9] values
+ */
+
 const find = (numbers) => {
-  return numbers
+  return [Math.min(...numbers), Math.max(...numbers)]
 }
 
 exports.find = find
