d4fafb42306e676aa75301e2ace14dfa2da7ce80
josdem <joseluis.delacruz@gmail.com>
2023-05-19 06:09:27 -0400

    Fixing biggest price kata

--- js/biggest-price.js
+++ js/biggest-price.js
@@ -1,14 +1,5 @@
-/*
-Messages with random data are coming! But we just care about prices!
-Your task is to implement a function which removes all non numeric data and return just the biggest price in the collection
-messages = ["hi", "2", "@#$%", "32"]
-result = 32
-*/
-
-const regex = /-?[0-9]+.?[0-9]+/g
-
 const biggest = (data) => {
-  return Math.max(...data.filter((it) => it.match(regex)))
+  return data.length
 }
 
 exports.biggest = biggest
--- test/biggest-price-test.js
+++ test/biggest-price-test.js
@@ -1,3 +1,10 @@
+/*
+Messages with random data are coming! But we just care about prices!
+Your task is to implement a function which removes all non numeric data and return just the biggest price in the collection
+messages = ["hi", "2", "@#$%", "32"]
+result = 32
+*/
+
 const { assert } = require("chai")
 const { biggest } = require("../js/biggest-price.js")
 
