2a9add406670bda259f2544bb81277be77c23ff3
josdem <joseluis.delacruz@gmail.com>
2023-05-19 06:47:24 -0400

    Moving kata description to the test

--- js/longest.substring.js
+++ js/longest.substring.js
@@ -1,14 +1,3 @@
-/**
- * Given A string
- * When I call reduce method
- * Then I should have the length of a new string without repeated characters
- *
- * Examples:
- * 1. abcabcbb -> abc = 3
- * 2. bbbbb -> b = 1
- * 3. ABDEFGABEF -> BDEFGA = 6
- */
-
 const longest = (data) => {
   data.length
 }
