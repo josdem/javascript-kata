fc1e01b494e9f7ca68a2430101a114c19619f922
josdem <joseluis.delacruz@gmail.com>
2023-05-20 13:31:45 -0400

    Fixing file names

---
+++ js/counting.characters.js
@@ -0,0 +1,12 @@
+const countVowels = (string) => {
+  return string.length
+}
+
+const countConsonants = (string) => {
+  return string.length
+}
+
+module.exports.counter = {
+  countVowels,
+  countConsonants,
+}
--- test/counting.characters.test.js
+++ test/counting.characters.test.js
@@ -1,5 +1,12 @@
+/*
+ * Counting vowels and consonants. Create two functions one for counting vowels and another for counting consonants
+ * Given: A string
+ * When: That string is josdem
+ * Then: Counting vowels should be 2 and consonants 4
+ */
+
 const { assert } = require("chai")
-const { counter } = require("../js/counting-characters")
+const { counter } = require("../js/counting.characters")
 
 describe("should count vowels and consonants", () => {
   const nickname = "josdem"
