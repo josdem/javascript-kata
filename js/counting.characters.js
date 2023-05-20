52e99a9b5f3eaa9b7062ded10dd1cda56d9b7537
josdem <joseluis.delacruz@gmail.com>
2023-05-20 13:35:37 -0400

    Adding counting characters kata solution

--- js/counting.characters.js
+++ js/counting.characters.js
@@ -1,9 +1,12 @@
+const vowels = ['a', 'e', 'i', 'o', 'u']
+const consonants  = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n','r', 'p', 'q', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
 const countVowels = (string) => {
-  return string.length
+  return Array.from(string).filter((letter) => vowels.includes(letter)).length
 }
 
 const countConsonants = (string) => {
-  return string.length
+  return Array.from(string).filter((letter) => consonants.includes(letter)).length
 }
 
 module.exports.counter = {
