67bdf7c7e2abe2e06ed8bd6d4281d23b8539aa33
josdem <joseluis.delacruz@gmail.com>
2023-05-20 13:11:14 -0400

    Adding common elements kata

---
+++ js/common.elements.js
@@ -0,0 +1,5 @@
+const finder = (first, second) => {
+  return first.concat(second)
+}
+
+exports.finder = finder
--- test/common.elements.test.js
+++ test/common.elements.test.js
@@ -1,5 +1,10 @@
+/*
+I have two arrays with integers. I want to return elements in common.
+Given: [1,2,3,4,5] and [1,3,5,7,9] then expected output: [1,3,5]
+*/
+
 const { assert } = require("chai")
-const { finder } = require("../js/common-elements")
+const { finder } = require("../js/common.elements")
 
 describe("should select elements in common", () => {
   it("filters items in common", () => {
