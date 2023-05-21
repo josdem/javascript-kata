fdf0172c232d62f07101f761b9cc4a2cc8af2a38
josdem <joseluis.delacruz@gmail.com>
2023-05-20 20:47:18 -0400

    Fixing differentiator kata

--- test/differentiator.test.js
+++ test/differentiator.test.js
@@ -1,3 +1,8 @@
+/*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+It should remove all values from list a, which are present in list b keeping their order.
+ */
+
 const { assert } = require("chai")
 const { diff } = require("../js/differentiator.js")
 
