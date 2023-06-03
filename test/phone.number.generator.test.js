f96a522860b8b5ad1395398ae3674d6c8ea287d7
josdem <joseluis.delacruz@gmail.com>
2023-06-02 20:50:57 -0400

    Deleting phone number generator solution

---
+++ test/phone.number.generator.test.js
@@ -0,0 +1,14 @@
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+*/
+
+const { assert } = require("chai")
+const { generate } = require("../js/phone.number.generator.js")
+
+describe("generate phone numbers", () => {
+  it("should format numbers", () => {
+    assert.strictEqual(generate([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+    assert.strictEqual(generate([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111")
+    assert.strictEqual(generate([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890")
+  })
+})
