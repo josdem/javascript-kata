c9e9e02926819590ed795d21198f73a4cb1ad7ac
josdem <joseluis.delacruz@gmail.com>
2023-06-03 07:54:09 -0400

    Adding string compresor solution

--- js/string.compressor.js
+++ js/string.compressor.js
@@ -1,5 +1,15 @@
 const compress = (keyword) => {
-  return keyword
+  let result = ""
+  const map = new Map()
+  Array.from(keyword).reduce((acumulator, it) => {
+    let counter = Array.from(keyword).filter((number) => number == it).length
+    map.set(it, counter)
+    acumulator = it
+  })
+  map.forEach((key, value) => {
+    result = result.concat(value, key)
+  })
+  return result
 }
 
 exports.compress = compress
