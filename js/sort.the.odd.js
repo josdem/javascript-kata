887560bb7be481356b298a369745d71dfb904cf6
josdem <joseluis.delacruz@gmail.com>
2023-06-03 07:29:53 -0400

    Adding sort the odd solution

--- js/sort.the.odd.js
+++ js/sort.the.odd.js
@@ -1,5 +1,6 @@
 const sort = (array) => {
-  return array
+  const odds = array.filter((element) => element % 2).sort((a, b) => a - b)
+  return array.map((element) => (element % 2 ? odds.shift() : element))
 }
 
 exports.sorter = sort
