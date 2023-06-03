a31b112bfec53f6cde17fef132e48b3ac0502406
josdem <joseluis.delacruz@gmail.com>
2023-06-02 20:46:47 -0400

    Adding most popular solution

--- js/most.popular.js
+++ js/most.popular.js
@@ -1,5 +1,14 @@
 const find = (numbers) => {
-  return numbers.length
+  let max = 0
+
+  return numbers.reduce((accomulator, it) => {
+    let counter = numbers.filter((number) => number === it).length
+    if (counter > max) {
+      max = counter
+      accomulator = it
+    }
+    return accomulator
+  }, 0)
 }
 
 exports.find = find
