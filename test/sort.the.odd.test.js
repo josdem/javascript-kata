b798714153b1373d7b605e985169629ed34ed79e
josdem <joseluis.delacruz@gmail.com>
2023-06-03 07:27:11 -0400

    Adding missing files

--- test/sort.the.odd.test.js
+++ test/sort.the.odd.test.js
@@ -1,3 +1,7 @@
+/*
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+*/
+
 const { assert } = require("chai")
 const { sorter } = require("../js/sort-the-odd.js")
 
